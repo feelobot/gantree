f8a7da4c43e36c1f6043b0393cd158a6f1018a4c
feelobot <felix.a.rod@gmail.com>
2014-08-28 21:48:08 -0700

    able to deploy app

--- lib/gantree/cli.rb
+++ lib/gantree/cli.rb
@@ -1,24 +1,68 @@
 require 'thor'
+require 'aws-sdk'
 require 'gantree/cli/help'
 
 module Gantree
 
   class CLI < Thor
+    AWS.config(
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCES_KEY'])
+
     class_option :verbose, :type => :boolean
-
-    desc "hello NAME", "say hello to NAME"
-    option :from, :desc => 'from person'
-    def hello(name)
-      puts "from: #{options[:from]}" if options[:from]
-      puts "Hello #{name}"
+    desc "deploy APP", "deploy specified APP"
+    option :branch, :desc => 'branch to deploy'
+    option :tag,    :desc => 'set image tag'
+    def deploy(app)
+      @env = app
+      @app = app.match(/^*\-(.*\-).*\-/)[1][0..-2]
+      @version_label = set_version_label
+      @eb = AWS::ElasticBeanstalk::Client.new
+      puts "Deploying #{app}"
+      upload_to_s3(app + "-versions")
+      create_version
+      update_application
     end
 
-    desc "fetch <repository> [<refspec>...]", "Download objects and refs from another repository"
-    options :all => :boolean, :multiple => :boolean
-    option :append, :type => :boolean, :aliases => :a, :desc => 'desc'
-    def fetch(respository, *refspec)
-      # implement git fetch here
+    private
+
+    def upload_to_s3 (dir)
+      s3 = AWS::S3.new
+      filename = @version_label
+      FileUtils.cp("Dockerrun.aws.json", filename)
+      key = File.basename(filename)
+      s3.buckets[dir].objects[key].write(:file => filename)
+      FileUtils.rm(filename)
     end
- 
+
+    def create_version
+      begin
+        @eb.create_application_version({
+          :application_name => @app,
+          :version_label => "#{@app}-#{@version_label}",
+          :source_bundle => {
+            :s3_bucket => "#{@app}-versions",
+            :s3_key => @version_label
+          }
+        })
+      rescue AWS::ElasticBeanstalk::Errors::InvalidParameterValue
+        puts "Version not created, already exists"
+      end
+    end
+
+    def update_application
+      @eb.update_environment({
+        :environment_name => @env,
+        :version_label => "#{@app}-#{@version_label}"
+      })
+
+    end
+
+    def set_version_label
+      branch = `git branch`
+      branch = branch[2..-1]
+      hash = `git rev-parse --verify --short #{branch}`.strip
+      "#{hash}-Dockerrun.aws.json"
+    end
   end
 end
---
+++ lib/gantree/cli/deploy.rb
@@ -0,0 +1,16 @@
+ require 'thor'
+require 'gantree/cli/help'
+
+module Gantree
+
+  class CLI < Thor
+    class_option :verbose, :type => :boolean
+
+    desc "deploy APP", "deploy specified APP"
+    option :branch, :desc => 'branch to deploy'
+    option :tag,    :desc => 'set image tag'
+    def deploy(app)
+      puts "Deploying #{app}"
+    end
+  end
+e
