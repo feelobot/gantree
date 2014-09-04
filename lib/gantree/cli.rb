9090e02bbeeda993b812a80a749edac8329392b7
feelobot <felix.a.rod@gmail.com>
2014-09-04 15:38:59 -0700

    create option to initialize a dockerrun

--- lib/gantree/cli.rb
+++ lib/gantree/cli.rb
@@ -4,21 +4,19 @@ require 'gantree/cli/help'
 
 module Gantree
   class CLI < Thor
-    AWS.config(
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCES_KEY'])
 
     desc "deploy APP", "deploy specified APP"
     option :branch, :desc => 'branch to deploy'
     method_option :tag, :aliases => "-t", :desc => "Set docker tag to deploy"
     def deploy app
-      Gantree::Deploy.new(options.clone).run
+      Gantree::Deploy.new(app, options).run
     end
 
     desc "init IMAGE", "create a dockerrun for your IMAGE"
     method_option :user, :aliases => "-u", :desc => "user credentials for private repo"
+    method_option :port, :aliases => "-p", :desc => "port of running application"
     def init image
-      Gantree::Init.new(options.clone).run
+      Gantree::Init.new(image,options).run
     end
   end
 end
--- lib/gantree/init.rb
+++ lib/gantree/init.rb
@@ -4,12 +4,55 @@ require 'aws-sdk'
 module Gantree
 
   class Init
-    def initialize options
+    def initialize image,options
+      @image = image
       @options = options
+      @s3 = AWS.s3.new
     end
 
     def run
-      puts "initialize image #{@options.image}"
+      puts "initialize image #{@image}"
+      puts "with user #{@options.user}" if @options.user
+      FileUtils.rm("Dockerrun.aws.json") if File.exist?("Dockerrun.aws.json")
+      create_docker_config_folder
+      create_dockerrun
+      upload_docker_config if @options.user
     end
+
+    def create_docker_config_folder
+      bucket = @s3.buckets.create("docker-cfgs")
+    end
+
+    def dockerrun_object
+      docker = {
+        AWSEBDockerrunVersion: "1",
+        Image: {
+          Name: @image,
+          Update: true
+        },
+        Ports: [
+          {
+            "ContainerPort": @options.port
+          }
+        ]
+      }
+      if @options.user
+        docker.Authentication = {
+          "Bucket": "docker-cfgs",
+          "Key": "#{@options.user}.dockercfg"
+        }
+      end
+    end
+
+    def create_dockerrun
+      IO.write("Dockerrun.aws.json",JSON.pretty_generate(dockerrun_object))
+    end
+
+    def upload_docker_config
+      FileUtils.cp("~/.dockercfg", "~/#{@options.user}.dockercfg")
+      key = File.basename("~/#{@options.user}.dockercfg")
+      @s3.buckets["docker-cfgs"].objects[key].write(:file => filename)
+    end
+
   end
 end
