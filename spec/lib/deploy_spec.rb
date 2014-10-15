9a62b35931b258049ffde93328d60b134e5e69dd
Felix Rodriguez <felix.a.rod@gmail.com>
2014-10-15 15:01:06 -0700

    fix edgecases for version labels that are not unique

--- spec/lib/deploy_spec.rb
+++ spec/lib/deploy_spec.rb
@@ -12,17 +12,19 @@ describe "#Deploy" do
 
   describe ".get_ext_branch" do
     it "returns branch name of repo url" do
-      options = { ext: "git@github.com:br/.ebextensions:feature" }
+      options = { ext: "git@github.com:br/.ebextensions:basic" }
       deploy = Gantree::Deploy.new(@env,options)
-      expect(deploy.instance_eval { get_ext_branch }).to eq "feature"
+      expect(deploy.instance_eval { get_ext_branch }).to eq "basic"
     end
   end
   
   describe ".get_ext_repo" do 
     it "returns just the repo url" do 
-      options = { ext: "git@github.com:br/.ebextensions:feature" }
+      options = { ext: "git@github.com:br/.ebextensions:basic" }
       deploy = Gantree::Deploy.new(@env,options)
       puts deploy.instance_eval { get_ext_repo } 
     end
-  end 
+  end
+
+  describe ".create_"
 end
