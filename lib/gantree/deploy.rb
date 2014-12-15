801efa4d847eb0caa3cb156f82b7472ab0d82f67
Felix Rodriguez <felix.a.rod@gmail.com>
2014-12-15 12:58:02 -0800

    deploy class refactor

--- lib/gantree/deploy.rb
+++ lib/gantree/deploy.rb
@@ -19,20 +19,11 @@ module Gantree
     def run
       if application?
         puts "Found Application: #{@name}".green
-        environments = eb.describe_environments({ :application_name => @app })[:environments]
-        if environments.length > 1
-          puts "WARN: Deploying to All Environments in the Application: #{@name}".yellow
-          sleep 3
-          envs = []
-          environments.each do |env|
-            envs << env[:environment_name]
-          end
-          puts "envs: #{envs}"
-          deploy(envs)
-        elsif environments.length == 1
-          env = environments.first[:environment_name]
-          puts "Found Environment: #{env}".green
-          deploy([env])
+        @environments = eb.describe_environments({ :application_name => @app })[:environments]
+        if multiple_environments?
+          deploy_to_all
+        elsif environment_found?
+          deploy_to_one
         else
           puts "ERROR: There are no environments in this application".red
           exit 1
@@ -56,6 +47,31 @@ module Gantree
         @app = results[:applications][0][:application_name]
         return true
       end
+    end
+
+    def multiple_environments?
+      @environments.length > 1 ? true : false
+    end
+
+    def environment_found?
+      @environments.length >=1 ? true : false
+    end
+
+    def deploy_to_all
+      puts "WARN: Deploying to All Environments in the Application: #{@name}".yellow
+      sleep 3
+      envs = []
+      @environments.each do |env|
+        envs << env[:environment_name]
+      end
+      puts "envs: #{envs}"
+      deploy(envs)
+    end
+
+    def deploy_to_one
+      env = @environments.first[:environment_name]
+      puts "Found Environment: #{env}".green
+      deploy([env])
     end
 
     def environment?
