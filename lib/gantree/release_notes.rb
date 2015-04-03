7c1c302be2976fbef3573df52b2bc81f7551f3bf
Felix Rodriguez <felix.a.rod@gmail.com>
2015-04-03 14:12:07 -0700

    think its ready now

--- lib/gantree/release_notes.rb
+++ lib/gantree/release_notes.rb
@@ -1,8 +1,8 @@
 require 'aws-sdk'
 module Gantree
   class ReleaseNotes
-    def initializer wiki, app, hash
-      @app = app
+    def initialize wiki, app, hash
+      @application = app
       @new_hash = hash
       @wiki_url = wiki
     end 
@@ -10,7 +10,7 @@ module Gantree
     def create
       get_release_notes
       write_release_notes
-      #commit_release_notes
+      commit_release_notes
     end
     
     def get_release_notes
@@ -20,13 +20,16 @@ module Gantree
     end
     
     def write_release_notes
-      release_notes_file = "Release-notes-br-#{@app}.md"
-      `echo "#{release_notes}" | cat - #{release_notes_file} > #{release_notes_file}.tmp  && mv #{release_notes_file}.tmp #{releas_notes_file}`
+      wiki_dir = "/tmp/wiki_release_notes/"
+      release_notes_file = "Release-notes-br-#{@application}.md"
+      path_to_wiki_file = "#{wiki_dir}#{release_notes_file}"
+      `touch #{path_to_wiki_file}` unless File.exist? "#{path_to_wiki_file}"
+      `echo "#{release_notes}" | cat - #{path_to_wiki_file} > #{path_to_wiki_file}.tmp  && mv #{path_to_wiki_file}.tmp #{path_to_wiki_file}`
     end
 
     def release_notes
       time = Time.now.strftime("%a, %e %b %Y %H:%M:%S %z")
-      "#{time} [#{last_deployed_hash}...#{@new_hash}](https://github.com/br/#{app}/compare/#{last_deployed_hash}...#{@new_hash})"
+      "#{time} [#{last_deployed_hash}...#{@new_hash}](https://github.com/br/#{@application}/compare/#{last_deployed_hash}...#{@new_hash})"
     end
 
     def last_deployed_hash
@@ -46,6 +49,10 @@ module Gantree
         raise "No Master Tags Deployed:\n #{resp["application_versions"].inspect}"
         500
       end
+    end
+    def commit_release_notes
+      `cd /tmp/wiki_release_notes && git add . && git commit -am "Updated release notes" && git push origin master`
+    end
     end
   end
 end
