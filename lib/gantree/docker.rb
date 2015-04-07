2731ab08d69f799066dc927a6aa5890e2b7a4c27
Felix Rodriguez <felix.a.rod@gmail.com>
2015-04-07 16:52:37 -0700

    pull images before doing any gantree ships

--- lib/gantree/docker.rb
+++ lib/gantree/docker.rb
@@ -10,6 +10,18 @@ module Gantree
       @image_path = @options[:image_path]
       raise "Please provide an image path name in .gantreecfg ex. { 'image_path' : 'bleacher/cms' }" unless @image_path
       @tag = @options[:tag] ||= tag
+      @base_image_tag = @options[:base_image_tag]
+    end
+    
+    def pull
+      puts "Pulling Image First..."
+      if @base_image_tag
+        puts "Pulled Image: #{@image_path}:#{@base_image_tag}".green if system("docker pull #{i@image_path}:#{@base_image_tag}")
+      elsif system("docker pull #{@image_path}")
+        puts "Pulled Image: #{@image_path}".green
+      else 
+        puts "Failed to Pull Image #{@image_path}".red
+      end
     end
 
     def build
